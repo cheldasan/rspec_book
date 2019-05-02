38c196e5a0081f917f6a087dc5ccf96194ed0825
chelda <chelda3230@gmail.com>
2019-05-02 21:05:38 +0900

    fix rspec and delete deprecation warning about 'should_receive'

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -3,18 +3,16 @@ require 'spec_helper'
 module Codebreaker
   describe Game do
     describe "#start" do
-      before(:each) do
-        let(:output) { double('output').as_null_object }
-        let(:game) { Game.new(@output) }
-      end
+      let(:output) { double('output').as_null_object }
+      let(:game) { Game.new(output) }
 
       it "sends a welcome message" do
-        output.should_receive(:puts).with('Welcome to Codebreaker!')
+        output.expect_with(:puts).with('Welcome to Codebreaker!')
         game.start
       end
 
       it "prompts for the first guess" do
-        output.should_receive(:puts).with('Enter guess:')
+        output.expect_with(:puts).with('Enter guess:')
         game.start
       end
     end
